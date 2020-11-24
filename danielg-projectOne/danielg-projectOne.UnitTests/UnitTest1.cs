1ca092d2cf33a7fa9fd0c2c089c4a36f4ac8afaa
dannydjg16 <dannydjg16@gmail.com>
2020-11-23 21:58:44 -0500

    Added test project and changed the title of my webpages

---
+++ danielg-projectOne/danielg-projectOne.UnitTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace danielg_projectOne.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
