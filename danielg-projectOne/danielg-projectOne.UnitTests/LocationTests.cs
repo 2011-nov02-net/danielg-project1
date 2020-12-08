401408c5a891834666181dcf9c9afd8a3860977e
dannydjg16 <dannydjg16@gmail.com>
2020-12-07 21:20:04 -0500

    Added tests

---
+++ danielg-projectOne/danielg-projectOne.UnitTests/LocationTests.cs
@@ -0,0 +1,35 @@
+﻿using danielg_projectOne.Library;
+using Xunit;
+
+
+namespace danielg_projectOne.UnitTests
+{
+    public class LocationTests
+    {
+        [Theory]
+        [InlineData(-1)]
+        [InlineData(-2)]
+        [InlineData(-193)]
+        public void TestKeepInventoryNotNegativeFail(int value)
+        {
+            var location = new Location();
+
+            bool tf = location.CheckInventoryValid(value);
+
+            Assert.False(tf, "Inventory should always be positive or 0");
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(19)]
+        [InlineData(10754)]
+        public void TestKeepInventoryNotNegativePass(int value)
+        {
+            var location = new Location();
+
+            bool tf = location.CheckInventoryValid(value);
+
+            Assert.True(tf, "Inventory should always be positive or 0");
+        }
+    }
+}
---
+++ danielg-projectOne/danielg-projectOne.UnitTests/OrderTests.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections.Generic;
+using danielg_projectOne.Library;
+using danielg_projectOne.Library.Order;
+using Xunit;
+
+
+namespace danielg_projectOne.UnitTests
+{
+    public class OrderTests
+    {
+
+        [Fact]
+        public void TestCartWillFillFail()
+        {
+            var order = new Order();
+
+            Product prod1 = new Product("Dumb Big Mac", 2);
+
+            order.AddToCustomerCart(prod1, 1);
+
+            bool filled = order.Customer.ShoppingCart["Dumb Big Mac"] == 2;
+
+            Assert.False(filled, "Cart was not succesfully filled");
+        }
+
+        [Fact]
+        public void TestCartWillFillPass()
+        {
+            var order = new Order();
+
+            Product prod1 = new Product("Dumb Big Mac", 2);
+
+            order.AddToCustomerCart(prod1, 1);
+
+            bool filled = order.Customer.ShoppingCart["Dumb Big Mac"] == 1;
+
+            Assert.True(filled, "Cart was succesfully filled");
+        }
+
+
+        [Fact]
+        public void TestGetOrderTotal()
+        {
+            var cart = new Dictionary<string, int>()
+            {
+                { "Dumb Big Mac", 1}
+            };
+            var customer = new CustomerClass(cart);
+
+            var order = new Order(customer);
+
+            Product prod1 = new Product("Dumb Big Mac", 3);
+
+            var amt = order.CalaculateTotalOfOneProduct(prod1);
+
+            bool equal = amt == 3;
+
+            Assert.True(equal, "Price should be 3");
+        }
+
+        [Fact]
+        public void TestGetOrderTotalFail()
+        {
+            var cart = new Dictionary<string, int>()
+            {
+                { "Dumb Big Mac", 1}
+            };
+            var customer = new CustomerClass(cart);
+
+            var order = new Order(customer);
+
+            Product prod1 = new Product("Dumb Big Mac", 3);
+
+            var amt = order.CalaculateTotalOfOneProduct(prod1);
+
+            bool equal = amt == 3;
+
+            Assert.False(equal, "Price should not be 3");
+        }
+
+
+    }
+}
